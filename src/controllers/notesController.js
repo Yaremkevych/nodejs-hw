5d511ce3d275be64b99b9d7185cd2fff7e1b5b43
Yaremkevych <137343042+Yaremkevych@users.noreply.github.com>
2026-06-03 23:58:47 +0200

    fix

--- src/controllers/notesController.js
+++ src/controllers/notesController.js
@@ -24,7 +24,7 @@ export const createNote = async (req, res) => {
 };
 
 //delete
-export const deleteNoteById = async (req, res) => {
+export const deleteNote = async (req, res) => {
   const { noteId } = req.params;
   const note = await Note.findOneAndDelete({ _id: noteId });
   if (!note) {
--- src/routes/notesRoutes.js
+++ src/routes/notesRoutes.js
@@ -1,7 +1,7 @@
 import { Router } from 'express';
 import {
   createNote,
-  deleteNoteById,
+  deleteNote,
   getAllNotes,
   getNoteById,
   updateNote,
@@ -12,7 +12,7 @@ const router = Router();
 router.get('/notes', getAllNotes);
 router.get('/notes/:noteId', getNoteById);
 router.post('/notes', createNote);
-router.delete('/notes/:noteId', deleteNoteById);
+router.delete('/notes/:noteId', deleteNote);
 router.patch('/notes/:noteId', updateNote);
 
 export default router;
